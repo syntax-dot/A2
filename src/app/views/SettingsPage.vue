c4e998381933cb9b08abd00bc180d8ab564e6e2b
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-12-07 20:30:16 +0300

    wip

--- src/app/views/SettingsPage.vue
+++ src/app/views/SettingsPage.vue
@@ -1,6 +1,8 @@
 <template>
   <div :class="$style.root">
-    <OtherSettings :locklentaupdate="user.locklentaupdate"/>
+    <OtherSettings :locklentaupdate="user.locklentaupdate"
+                   :selectedTimezone="user.timezone"/>
+
     <SaveButton/>
   </div>
 </template>
@@ -13,34 +15,11 @@ import { getUser } from '../../api/getUser'
 import { OtherSettings } from '../components/OtherSettings'
 import { User } from '../types/User'
 
-// const axios = new Axios()
-
-// const api = 'https://api.av100.ru/v3/login'
-// const xApiKey = '8bcfb6e1-4fa8-4fae-872c-a435bbdbe8d9'
-// const xUserToken = '5ae041ae-4ecf-46c8-a7f6-8a64249fa3cc'
-
-// onMounted(async () => {
-//   const user = await postLogin({
-//     login: '9852158891',
-//     password: '6030616154',
-//   })
-//   localStorage.setItem('userToken', user.token)
-//   localStorage.setItem('userId', user.user.id)
-// })
-
-// onMounted(async () => {
-//   const userId = localStorage.getItem('userId')
-//   if (!userId)
-//     return
-
-//   const user = await getUserUserId(+userId)
-//   console.log(user)
-// })
-
 const user = ref<User>()
 
 onMounted(async () => {
   await login()
+
   const response = await getUser()
   if (response)
     user.value = response
