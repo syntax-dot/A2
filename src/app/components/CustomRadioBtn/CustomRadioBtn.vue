27f54a14d1fc2c8d7b64ca4c003b2f5ef95670b6
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-12-08 21:26:13 +0300

    wip

--- src/app/components/CustomRadioBtn/CustomRadioBtn.vue
+++ src/app/components/CustomRadioBtn/CustomRadioBtn.vue
@@ -5,17 +5,57 @@
            :class="$style.custom_radio"
            name="radio">
     <label :class="$style.label"
-           for="radio">Custom</label>
+           for="radio">
+      Custom
+    </label>
+
+    <img v-show="!inputShow"
+         :class="$style.edit"
+         src="../../../assets/icons/edit.svg"
+         alt="edit"
+         @click="inputShow = !inputShow">
+
+    <input v-show="inputShow"
+           :class="$style.input"
+           type="text">
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref, onMounted, onUnmounted } from 'vue'
 
+const inputShow = ref(false)
+
+const input = ref<HTMLInputElement>()
+
+onMounted(() => {
+  window.addEventListener('click', handleClick)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('click', handleClick)
+})
+
+function handleClick(e: Event) {
+  if (e.target instanceof HTMLElement) {
+    let element = e.target
+
+    while (element.parentElement !== null) {
+      if (element === input.value)
+        return
+
+      element = element.parentElement
+    }
+
+    inputShow.value = false
+  }
+}
 </script>
 
 <style module lang="scss">
 .root {
-
+  display: flex;
+  justify-content: space-between;
 }
 
 .custom_radio {
@@ -51,6 +91,12 @@
 }
 
 .label {
+  cursor: pointer;
+}
+
+.edit {
+  width: 2.2rem;
+  height: 2.2rem;
   cursor: pointer;
 }
 </style>
