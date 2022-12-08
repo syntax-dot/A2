28ad1210875024697e499e4da6a66d08fb894fa6
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-12-08 20:38:30 +0300

    wip

---
+++ src/app/components/CustomRadioBtn/CustomRadioBtn.vue
@@ -0,0 +1,19 @@
+<template>
+  <div :class="$style.root">
+    <input id="radio"
+           type="radio"
+           class="custom-radio"
+           name="radio">
+    <label for="radio">Custom</label>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style module lang="scss">
+.root {
+
+}
+</style>
---
+++ src/app/components/CustomRadioBtn/index.ts
@@ -0,0 +1,1 @@
+export { default as CustomRadioBtn } from './CustomRadioBtn.vue'
