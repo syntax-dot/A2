39fc29b6d9f4bda8b09e00ecea4151b15021c1ff
Aleksandr <75541723+SHKUROLOG@users.noreply.github.com>
2022-12-09 19:15:18 +0300

    wip

--- src/app/components/NotificationFields/NotificationFields.vue
+++ src/app/components/NotificationFields/NotificationFields.vue
@@ -2,7 +2,8 @@
   <div :class="$style.root">
     <h4>Уведомления</h4>
     <div>
-      <CustomRadioBtn v-for="notify in Notify"/>
+      <CustomRadioBtn v-for="notify in Notify"
+                      :key="notify"/>
     </div>
   </div>
 </template>
